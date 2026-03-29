ac49b3a3a856414236bf872e0688e63572c6fa9f
Devin J Cornell <devinjcornell@gmail.com>
2026-03-29 10:51:36 -0400

    updated mongo models

--- server/app/mongo_models/__init__.py
+++ server/app/mongo_models/__init__.py
@@ -1,5 +1,5 @@
 from .book_manager import BookManager
 
-from .book_research import BookCollection, BookDoc, BookResearchWithSimilarity
-from .research_task import ResearchTaskCollection, ResearchTaskDoc, TaskStatus
+from .book_collection import BookCollection, BookDoc, BookResearchWithSimilarity
+from .research_task_collection import ResearchTaskCollection, ResearchTaskDoc, TaskStatus
 
